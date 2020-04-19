451266168b339898323d41cb402c23f069e1cba6
Stevenson Marquez <stevensonxmarquez@gmail.com>
2020-04-19 12:12:20 -0500

    update item struct

--- pkg/api/models/picture.go
+++ pkg/api/models/picture.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Picture struct {
-	ID   primitive.ObjectID `bson:"_id" json:"id"`
-	Src  string             `json:"src"`
-	Data primitive.M        `json:"data"`
+	Reference string      `json:"reference"`
+	Src       string      `json:"src"`
+	Data      primitive.M `json:"data"`
 }
