fa7281b21c1ff305dfba764d86b17ae5a9452fab
Stevenson Marquez Rangel <smarquez@kuenta.co>
2020-05-16 17:24:18 -0500

    fix public config endpoint

--- pkg/api/routes/config_routes.go
+++ pkg/api/routes/config_routes.go
@@ -7,5 +7,5 @@ import (
 
 func config(r *mux.Router, mod *module) {
 	controller := &controllers.ConfigController{Config: mod.service.config}
-	r.HandleFunc("/api/v1/config/cloudinary", controller.Cloudinary).Methods("GET")
+	r.HandleFunc("/api/v1/public/config/cloudinary", controller.Cloudinary).Methods("GET")
 }
