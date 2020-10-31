f09e941a33938dfb5dfa8c851404c8ae7e2d459c
Stevenson Marquez Rangel <smarquez@kuenta.co>
2020-10-31 17:54:38 -0500

    parse funcs

--- pkg/core/app.go
+++ pkg/core/app.go
@@ -17,17 +17,18 @@ type App struct {
 // NewApp return new application instance
 func NewApp() (*App, error) {
 	config := load()
-	db, err := connect(config.MongoURI)
-	if err != nil {
-		return nil, fmt.Errorf("failed connecting to the database: %w", err)
-	}
 
-	templates, err := parseTemplates()
+	templates, err := parseTemplates(config)
 	if err != nil {
 		return nil, fmt.Errorf("failed parsing templates: %w", err)
 	}
 
 	slackClient := slack.New(config.Slack.AccessToken)
+
+	db, err := connect(config.MongoURI)
+	if err != nil {
+		return nil, fmt.Errorf("failed connecting to the database: %w", err)
+	}
 
 	app := &App{
 		DB:        db,
--- pkg/core/templates.go
+++ pkg/core/templates.go
@@ -2,7 +2,9 @@ package core
 
 import (
 	"fmt"
+	"github.com/dustin/go-humanize"
 	"html/template"
+	"strings"
 )
 
 type Templates struct {
@@ -10,12 +12,27 @@ type Templates struct {
 	Transport *template.Template
 }
 
-func parseTemplates() (*Templates, error) {
-	sale, err := template.ParseFiles("assets/templates/sale.html")
+var funcs = func(config *Config) template.FuncMap {
+	return template.FuncMap{
+		"first": func(name string) string {
+			return strings.Split(name, " ")[0]
+		},
+		"link": func(ref string) string {
+			return strings.ReplaceAll(config.SMTP.RefLink, "{ref}", ref)
+		},
+		"currency": func(value int) string {
+			return fmt.Sprintf("$%s", humanize.Comma(int64(value)))
+		},
+	}
+}
+
+func parseTemplates(config *Config) (*Templates, error) {
+	fn := funcs(config)
+	sale, err := template.New("sale").Funcs(fn).ParseFiles("assets/templates/sale.html")
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse sale template: %w", err)
 	}
-	transport, err := template.ParseFiles("assets/templates/transport.html")
+	transport, err := template.New("transport").Funcs(fn).ParseFiles("assets/templates/transport.html")
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse transport template: %w", err)
 	}
